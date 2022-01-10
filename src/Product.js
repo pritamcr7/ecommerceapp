6c6bb49303d62fb7d1c7d3477530e6e7cddd6d24
Pritam Chatterjee <pritamchatterjee2009@gmail.com>
2022-01-10 08:08:27 +0530

    Implementing Adding and Deleting Product Functionality

--- src/Product.js
+++ src/Product.js
@@ -7,8 +7,18 @@ import "bootstrap/dist/css/bootstrap.min.css";
 const Product = ({ name, id, price, available, imgsrc, vendor, category }) => {
   const [show, setShow] = useState(false);
 
+  const [counter, setCounter] = useState(available);
+
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
+  const handleIncrement = () => {
+    setCounter((prevCount) => prevCount + 1);
+  };
+
+  const handleDecrement = () => {
+    setCounter((prevCount) => prevCount - 1);
+  };
 
   return (
     <Card
@@ -30,40 +40,42 @@ const Product = ({ name, id, price, available, imgsrc, vendor, category }) => {
             <strong>{price}</strong>
           </p>
           <p className="product_vendor">{vendor}</p>
-          {available > 0 ? (
-            <p>Total Available Products - {available}</p>
+          {counter > 0 ? (
+            <p>Total Available Products - {counter}</p>
           ) : (
             <p className="product_notavailable">Out Of Stock</p>
           )}
         </Card.Text>
-        {available === 0 ? (
-          <Button variant="primary" disabled>
-            Purchase
-          </Button>
-        ) : (
-          <>
-            <Button variant="primary" onClick={handleShow}>
-              Purchase
+        <Button variant="secondary" onClick={handleIncrement}>
+          +
+        </Button>
+        <Button variant="primary" onClick={handleShow} disabled={counter === 0}>
+          Purchase
+        </Button>
+        <Button
+          variant="secondary"
+          disabled={counter === 0}
+          onClick={handleDecrement}
+        >
+          -
+        </Button>
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Do You want to Buy {name}</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <p>Vendor - {vendor}</p>
+            <p>Price - ₹{price}</p>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Close
             </Button>
-            <Modal show={show} onHide={handleClose}>
-              <Modal.Header closeButton>
-                <Modal.Title>Do You want to Buy {name}</Modal.Title>
-              </Modal.Header>
-              <Modal.Body>
-                <p>Vendor - {vendor}</p>
-                <p>Price - ₹{price}</p>
-              </Modal.Body>
-              <Modal.Footer>
-                <Button variant="secondary" onClick={handleClose}>
-                  Close
-                </Button>
-                <Button variant="primary" onClick={handleClose}>
-                  Yes
-                </Button>
-              </Modal.Footer>
-            </Modal>
-          </>
-        )}
+            <Button variant="primary" onClick={handleClose}>
+              Yes
+            </Button>
+          </Modal.Footer>
+        </Modal>
       </Card.Body>
     </Card>
   );
